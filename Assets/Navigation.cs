2efcba6504f57f5ba493e2746225344d0a2df813
amaneyn <mustafa_karakus_34@hotmail.com>
2014-04-19 12:47:28 +0300

    Determining which layer the *player is on

--- Assets/Navigation.cs
+++ Assets/Navigation.cs
@@ -8,22 +8,41 @@ public class Navigation : MonoBehaviour {
 
 	RaycastHit hit;
 
+	public string names=string.Empty;
+	public string names2=string.Empty;
+
 	// Use this for initialization
 	void Start () {
 		nav.angularSpeed = 500; 
 		nav.acceleration = 50; //Acceleration
-		nav.speed = 8; //Maximum speed
+		nav.speed = 9; //Maximum speed
+		nav.baseOffset = nav.transform.position.y;
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
+		//Determining the NavMesh Layer the player is on
+		int floorMask = 1 << NavMesh.GetNavMeshLayerFromName ("Floor");
+		NavMeshHit nHit;
+		nav.SamplePathPosition (-1, 0.0f, out nHit);
+
+		if ((nHit.mask & floorMask) != 0)
+		{
+			names = "Walkin in da hood";
+		}
+		else
+		{
+			names = "AW SHIT!";
+		}
+		//Determining the NavMesh Layer the player is on
+
 		if (nav.speed < 10) {
-			nav.speed +=  0.005f/17.5f; //For making it harder to control character as the game moves on
+			nav.speed +=  0.005f/17.5f; //For making it harder to control character as the game progesses
 		}
 
 		if (nav.acceleration >= 15) {
-			nav.acceleration -= 0.005f; //For making it harder to control character as the game moves on
+			nav.acceleration -= 0.005f; //For making it harder to control character as the game progesses
 		}
 
 		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition); 
@@ -32,7 +51,9 @@ public class Navigation : MonoBehaviour {
 			if (hit.collider.name == "TerrainMain") //Name of the floor Plane
 			{
 				if (Input.GetMouseButtonDown(1)){ //If right click
-					target.position = hit.point;
+					//target.position = hit.point;
+					Vector3 newPos = new Vector3(hit.point.x, hit.point.y + target.localScale.y/2, hit.point.z);
+					target.position=newPos;
 				}
 			}		
 		}
@@ -41,6 +62,7 @@ public class Navigation : MonoBehaviour {
 		if (Input.GetKeyDown (KeyCode.Space)) { //Fun shit. You be pressing...
 			nav.Warp(target.position); 			//...you be trippin
 		}
+	
 	}
 
 }
