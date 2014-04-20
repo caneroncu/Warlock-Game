48ac1c75602d4e6d4e4228f8dca59dce3d8ba663
amaneyn <mustafa_karakus_34@hotmail.com>
2014-04-20 17:08:25 +0300

    Added Lava Floor and basic RTS camera movement

---
+++ Assets/CameraMovement.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour {
+
+	public float CamSpeed;
+	public int GUISize;
+
+	// Use this for initialization
+	void Start () {
+		CamSpeed = 1.5f;
+		GUISize = 25;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var recdown = new Rect (0, 0, Screen.width, GUISize);
+		var recup = new Rect (0, Screen.height - GUISize, Screen.width, GUISize);
+		var recleft = new Rect (0, 0, GUISize, Screen.height);
+		var recright = new Rect (Screen.width - GUISize, 0, GUISize, Screen.height);
+
+		if (recdown.Contains (Input.mousePosition)) 
+		{
+			transform.Translate(0, 0, -CamSpeed, Space.World);
+		}
+		if (recup.Contains (Input.mousePosition)) 
+		{
+			transform.Translate(0, 0, CamSpeed, Space.World);
+		}
+		if (recleft.Contains (Input.mousePosition)) 
+		{
+			transform.Translate(-CamSpeed, 0, 0, Space.World);
+		}
+		if (recright.Contains (Input.mousePosition)) 
+		{
+			transform.Translate(CamSpeed, 0, 0, Space.World);
+		}
+
+	}
+}
--- Assets/Navigation.cs
+++ Assets/Navigation.cs
@@ -37,6 +37,7 @@ public class Navigation : MonoBehaviour {
 		}
 		//Determining the NavMesh Layer the player is on
 
+
 		if (nav.speed < 10) {
 			nav.speed +=  0.005f/17.5f; //For making it harder to control character as the game progesses
 		}
@@ -45,10 +46,11 @@ public class Navigation : MonoBehaviour {
 			nav.acceleration -= 0.005f; //For making it harder to control character as the game progesses
 		}
 
+
 		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition); 
 		if (Physics.Raycast (ray, out hit, Mathf.Infinity)) //Converting mouse position on screen to 3d vector
 		{
-			if (hit.collider.name == "TerrainMain") //Name of the floor Plane
+			if (hit.collider.name == "TerrainMain" || hit.collider.name == "TerrainLava") //Name of the floor Plane
 			{
 				if (Input.GetMouseButtonDown(1)){ //If right click
 					//target.position = hit.point;
@@ -58,6 +60,7 @@ public class Navigation : MonoBehaviour {
 			}		
 		}
 		nav.destination = target.position; //Player's destination is targer's position
+
 
 		if (Input.GetKeyDown (KeyCode.Space)) { //Fun shit. You be pressing...
 			nav.Warp(target.position); 			//...you be trippin
