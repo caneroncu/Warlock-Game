f92e471036d7b354c1d1768bc28afe039261ecfd
amaneyn <mustafa_karakus_34@hotmail.com>
2014-05-10 17:38:15 +0300

    Added new textures, skybox, lava flow. Limited camera movement.

--- Assets/CameraMovement.cs
+++ Assets/CameraMovement.cs
@@ -6,17 +6,20 @@ public class CameraMovement : MonoBehaviour {
 	public float CamSpeed;
 	public int GUISize;
 
-	public float sinus;
-	public float cosinus;
+	private float sinus;
+	private float cosinus;
+
 
 	// Use this for initialization
 	void Start () {
 		CamSpeed = 1.5f;
 		GUISize = 20;
+		FocusPlayer();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		LimitCamera();
 		var recdown = new Rect (0, 0, Screen.width, GUISize);
 		var recup = new Rect (0, Screen.height - GUISize, Screen.width, GUISize);
 		var recleft = new Rect (0, 0, GUISize, Screen.height);
@@ -42,16 +45,40 @@ public class CameraMovement : MonoBehaviour {
 		//Whenever the key is pressed down and HELD (Unlike "Input.GetKeyDown(KeyCode.Space)")
 		if (Input.GetKey (KeyCode.Space)) 
 		{
-			//Sinus and cosinus calculations of the camera angle
-			sinus = Mathf.Sin ((Mathf.PI / 180) * transform.eulerAngles.x);
-			cosinus = Mathf.Cos ((Mathf.PI / 180) * transform.eulerAngles.x);
-
-			GameObject player = GameObject.Find("Player");
-
-			//Used for determining the distance between camera and player on Z dimension
-			float cameraBias = transform.position.y / Mathf.Abs(sinus / cosinus);
-			transform.position = new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z-cameraBias); 
-
+			FocusPlayer();
 		}
+	}
+
+	private void LimitCamera()
+	{
+		if (transform.position.z <= -80) 
+		{
+			transform.position = new Vector3(transform.position.x, transform.position.y, -80);		
+		}
+		if (transform.position.z >= 30) 
+		{
+			transform.position = new Vector3(transform.position.x, transform.position.y, 30);		
+		}
+		if (transform.position.x <= -35) 
+		{
+			transform.position = new Vector3(-35, transform.position.y, transform.position.z);		
+		}
+		if (transform.position.x >= 35) 
+		{
+			transform.position = new Vector3(35, transform.position.y, transform.position.z);		
+		}
+	}
+
+	private void FocusPlayer()
+	{
+		//Sinus and cosinus calculations of the camera angle
+		sinus = Mathf.Sin ((Mathf.PI / 180) * transform.eulerAngles.x);
+		cosinus = Mathf.Cos ((Mathf.PI / 180) * transform.eulerAngles.x);
+		
+		GameObject player = GameObject.Find("Player");
+		
+		//Used for determining the distance between camera and player on Z dimension
+		float cameraBias = transform.position.y / Mathf.Abs(sinus / cosinus);
+		transform.position = new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z-cameraBias); 
 	}
 }
